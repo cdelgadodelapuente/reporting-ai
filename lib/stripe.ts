2780828fd6b241f8b32f79432e782a0ada1ad301
Christian Delgado <christiandelgado@ip-192-168-0-11.eu-west-1.compute.internal>
2026-02-09 12:56:51 +0100

    Update Stripe API version for SDK compatibility

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -3,6 +3,5 @@ import Stripe from "stripe";
 const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY || "";
 
 export const stripe = new Stripe(STRIPE_SECRET_KEY, {
-  apiVersion: "2024-12-18.acacia",
+  apiVersion: "2026-01-28.clover",
 });
-
